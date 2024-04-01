38690ab01dc50bf426666ee961c5cc97f3e349f8
noor uddin muhammad faateh <52114434+faateh77@users.noreply.github.com>
2024-04-01 17:41:36 +0500

    Add files via upload

---
+++ datatype.js
@@ -0,0 +1,47 @@
+//String
+var name = "asif";
+document.write(name);
+document.write("<br>");
+//Number
+var num = 13;
+document.write(num);
+document.write("<br>");
+//floating number
+var floatNum = 3.4
+document.write(floatNum)
+
+document.write("<br>");
+//Boolean
+var bool = true;
+document.write(bool);
+document.write("<br>");
+//undefined
+var a;
+document.write(a);
+document.write("<br>");
+//Null
+var b = null;
+document.write(b);
+document.write("<br>");
+//Array
+var things = ["big",12,'low','apple'];
+document.write(things);
+document.write("<br>");
+
+
+//Determining the type of variable 
+
+document.write(typeof(name));
+document.write("<br>");
+document.write(typeof(num));
+document.write("<br>");
+document.write(typeof(floatNum));
+document.write("<br>");
+document.write(typeof(bool));
+document.write("<br>");
+document.write(typeof(a));
+document.write("<br>");
+document.write(typeof(b));
+document.write("<br>");
+document.write(typeof(things));
+document.write("<br>");
